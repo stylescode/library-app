ffab62f51953065f1fb0d125b35b123fe6328101
Styles <stylesk11@gmail.com>
2023-11-16 15:53:19 -0800

    stub basic functions

---
+++ script.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+// create a book obj
+function Book() {
+
+}
+
+
+// take user input, create book and add to library
+function addBookToLibrary() {
+
+}
+
+// generate user form on click
+
+
+// loop through library and display each book
