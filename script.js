ecba33a11d0a7b18a97957fd27786b19e09eb82a
Styles <stylesk11@gmail.com>
2023-11-16 16:00:21 -0800

    add comments to script

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 const myLibrary = [];
 
 // create a book obj
-function Book() {
-
+function Book(title, author, year, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.year = year;
+  this.pages = pages;
+  this.read = read;
 }
 
 
 // take user input, create book and add to library
 function addBookToLibrary() {
+  // give user form to fill out
+  // use form info for parameters
+  // create Book
+  // add the book to library
 
 }
 
-// generate user form on click
+// func for user form
+
+
+// generate user form on click with listener
+
+
+// func to display a book
 
 
 // loop through library and display each book
